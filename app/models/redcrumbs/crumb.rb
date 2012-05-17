6e45236779393ff49feff669c21b1de25fbf3273
JonMidhir <info@midhirrecords.com>
2012-05-17 17:03:25 +0100

    Move properties back into the model

--- app/models/redcrumbs/crumb.rb
+++ app/models/redcrumbs/crumb.rb
@@ -10,9 +10,24 @@ module Redcrumbs
 
   class Crumb
     include DataMapper::Resource
-    include Crumb::Properties
     include Crumb::Getters
     include Crumb::Setters
+    
+    DataMapper.setup(:default, {:adapter  => "redis"})
+    
+    property :id, Serial
+    property :subject_id, Integer, :index => true
+    property :subject_type, String, :index => true
+    property :modifications, Json, :default => "{}"
+    property :created_at, DateTime
+    property :updated_at, DateTime
+    property :stored_creator, Json
+    property :stored_target, Json
+    property :stored_subject, Json
+    property :creator_id, Integer, :index => true
+    property :target_id, Integer, :index => true
+
+    DataMapper.finalize
 
     before :save, :convert_user_target_ids
 
