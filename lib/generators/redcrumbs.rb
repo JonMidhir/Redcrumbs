f2ea57af44f35aa10924f8052d9bc8c74a07ec77
JonMidhir <info@midhirrecords.com>
2012-05-17 13:58:50 +0100

    Try to namespace generator

--- lib/generators/redcrumbs.rb
+++ lib/generators/redcrumbs.rb
@@ -1,6 +1,6 @@
 module Redcrumbs
   class InstallGenerator < Rails::Generators::Base
-    source_root File.expand_path("../templates", __FILE__)
+    source_root File.expand_path("./templates", __FILE__)
 
     # all public methods in here will be run in order
     def add_redcrumbs_initializer
--- lib/redcrumbs/version.rb
+++ lib/redcrumbs/version.rb
@@ -1,3 +1,3 @@
 module Redcrumbs
-  VERSION = "0.0.2"
+  VERSION = "0.0.22"
 end
