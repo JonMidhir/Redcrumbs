8eb87de4b617dcce8c190209d1cb29889a05f36a
JonMidhir <info@midhirrecords.com>
2012-05-17 12:30:50 +0100

    Adds rest of the code and changes names of module/model etc

--- lib/redcrumbs.rb
+++ lib/redcrumbs.rb
@@ -1,5 +1,99 @@
 require "redcrumbs/version"
-
+require 'dm-core'
 module Redcrumbs
-  # Your code goes here...
+  mattr_accessor :creator_class_sym
+  mattr_accessor :creator_id
+  mattr_accessor :target_class_sym
+  mattr_accessor :target_id
+  
+  mattr_accessor :store_creator_attributes
+  mattr_accessor :store_target_attributes
+  
+  
+  def self.setup
+    yield self
+  end
+
+  def self.included(base)
+    base.extend(ClassMethods)
+  end
+  
+  def creator_class
+    self.creator_class_sym.to_s.classify.constantize
+  end
+  
+  def target_class
+    self.target_class_sym.to_s.classify.constantize
+  end
+  
+  module ClassMethods
+    def redcrumbed(opts = {})
+      
+      cattr_accessor :fields, :store, :if, :unless
+      self.fields = []
+      self.store = []
+      self.if = []
+      self.unless = []
+      self.fields += Array(opts[:only]) unless !opts[:only]
+      self.store += Array(opts[:store]) unless !opts[:store]
+      self.if += Array(opts[:if]) unless !opts[:if]
+      self.unless += Array(opts[:unless]) unless !opts[:unless]
+      
+      around_save :notify_changes, :if => self.if, :unless => self.unless
+      
+      include Redcrumbs::InstanceMethods
+    end
+  end
+  
+  module InstanceMethods
+    def notifications_for
+      Redcrumb.all(:target_id => self.zid, :order => [:created_at.desc])
+    end
+    
+    def notifications_by
+      Redcrumb.all(:user_id => self.zid, :order => [:created_at.desc])
+    end
+    
+    # This is an unforunate hack to get over the redis dm adapter's non-support of addition (OR) queries
+    def notifications_as_user(opts = {})
+      opts[:limit] ||= 100
+      arr = notifications_for 
+      arr += notifications_by
+      arr.all(:limit => opts[:limit])
+    end
+    
+    def notifications
+      Redcrumb.all(:subject_type => self.class.to_s, :subject_id => self.zid)
+    end
+    
+    def is?(conditionals)
+      eval(conditionals)
+    end
+    
+    def watched_changes
+      changes.reject {|k,v| !self.class.fields.include?(k.to_sym)}
+    end
+    
+    def storeable_attributes
+      attributes.reject {|k,v| !self.class.store.include?(k.to_sym)}
+    end
+    
+    def watched_changes_empty?
+       watched_changes.empty?
+    end
+    
+    def creator
+      send(Redcrumbs.creator_class_sym) if respond_to?(Redcrumbs.creator_class_sym)
+    end
+
+    private
+
+    def notify_changes
+      unless watched_changes.empty?
+        n = Redcrumb.build_from(self)
+        n.save
+      end
+      yield
+    end
+  end
 end
--- redcrumbs.gemspec
+++ redcrumbs.gemspec
@@ -17,4 +17,7 @@ Gem::Specification.new do |s|
   s.test_files    = `git ls-files -- {test,spec,features}/*`.split("\n")
   s.executables   = `git ls-files -- bin/*`.split("\n").map{ |f| File.basename(f) }
   s.require_paths = ["lib"]
+  
+  s.add_dependency 'data_mapper', '>= 1.2.0'
+  s.add_dependency 'dm-redis-adapter', '>= 0.6.2'
 end
