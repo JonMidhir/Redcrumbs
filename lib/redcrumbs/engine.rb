d3151f3babaf6508bd75815bf77d1425c41b8c94
JonMidhir <info@midhirrecords.com>
2012-05-17 15:23:09 +0100

    Add engine file

---
+++ lib/redcrumbs/engine.rb
@@ -0,0 +1,8 @@
+require "rails/engine"
+require "redcrumbs"
+
+module Redcrumbs
+  class Engine < Rails::Engine
+
+  end
+end
--- lib/redcrumbs/version.rb
+++ lib/redcrumbs/version.rb
@@ -1,3 +1,3 @@
 module Redcrumbs
-  VERSION = "0.0.31"
+  VERSION = "0.0.32"
 end
