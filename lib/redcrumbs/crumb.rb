c95c73b605e68ba744d06b1696f44e60459466f8
John Hope <info@midhirrecords.com>
2014-11-21 18:33:59 +0000

    Refactor Crumb model for reusability

--- lib/redcrumbs/crumb.rb
+++ lib/redcrumbs/crumb.rb
@@ -2,14 +2,13 @@ require 'dm-core'
 require 'dm-types'
 require 'dm-timestamps'
 require 'dm-redis-adapter'
-
-## Note to self: Syntax to grab all by an attribute is - Notification.all(:subject_type => "User")
-## The attribute must be indexed as with subject_type below
+require 'redcrumbs/serializable_association'
 
 module Redcrumbs
   class Crumb
     
     include DataMapper::Resource
+    include Redcrumbs::SerializableAssociation
     
     DataMapper.setup(:default, {:adapter  => "redis", :host => Redcrumbs.redis.client.host, :port => Redcrumbs.redis.client.port, :password => Redcrumbs.redis.client.password})
     
@@ -28,6 +27,9 @@ module Redcrumbs
     DataMapper.finalize
     
     after :save, :set_mortality
+
+    serializable_association(:creator)
+    serializable_association(:target)
 
     def initialize(params = {})
       self.subject = params[:subject]
@@ -72,88 +74,6 @@ module Redcrumbs
     
     def load_subject_from_storage
       @subject ||= subject_type.constantize.new(self.stored_subject, :without_protection => true)
-    end
-
-
-    # Assign additional creator attributes when changing the creator
-    #
-    def creator=(creator)
-      @creator = creator
-
-      store_creator_attributes and assign_creator_id
-    end
-    
-    def creator
-      @creator or creator_from_storage or full_creator
-    end
-    
-    def creator_class
-      Redcrumbs.creator_class_sym.to_s.classify.constantize
-    end
-
-    def creator_primary_key
-      Redcrumbs.creator_primary_key
-    end
-
-    def store_creator_attributes
-      self.stored_creator = if @creator
-        @creator.attributes.select {|k,v| Redcrumbs.store_creator_attributes.include?(k.to_sym)}
-      else
-        {}
-      end
-    end
-
-    def assign_creator_id
-      self.creator_id = @creator ? @creator[creator_primary_key] : nil
-    end
-    
-    def creator_from_storage
-      if self.stored_creator.present?
-        @creator_from_storage ||= creator_class.new(self.stored_creator, :without_protection => true)
-      end
-    end
-    
-    # grabbing full creator/target should memoize the result. Check to see is it a new_record (i.e. from storage) first
-    def full_creator
-      return nil unless creator_id
-
-      if @creator.blank? or @creator.new_record?
-        @creator = creator_class.where(creator_primary_key => self.creator_id).first
-      else
-        @creator
-      end
-    end
-
-    def target=(target)
-      return unless target
-
-      @target = target
-      self.stored_target = target.attributes.select {|attribute| Redcrumbs.store_target_attributes.include?(attribute.to_sym)}
-      self.target_id = target[Redcrumbs.target_primary_key]
-    end
-    
-    def target
-      if self.stored_target.present?
-        load_target_from_storage
-      elsif self.target_id.present?
-        full_target
-      end
-    end
-    
-    def load_target_from_storage
-      @target ||= target_class.new(self.stored_target, :without_protection => true)
-    end
-    
-    def target_class
-      Redcrumbs.target_class_sym.to_s.classify.constantize
-    end
-
-    def full_target
-      if @target.blank? or @target.new_record?
-        @target = target_class.where(Redcrumbs.target_primary_key => self.target_id).first
-      else
-        @target
-      end
     end
 
     def redis_key
---
+++ lib/redcrumbs/serializable_association.rb
@@ -0,0 +1,108 @@
+module Redcrumbs
+  module SerializableAssociation
+    def self.included(base)
+      base.extend(ClassMethods)
+    end
+
+    module ClassMethods
+      def serializable_association(name)
+        raise ArgumentError unless name and [:creator, :target].include?(name)
+
+        # Setter, e.g. object.creator=
+        #
+        define_method("#{name}=") do |associated|
+          instance_variable_set("@#{name}".to_sym, associated)
+
+          assign_id_for(name, associated)
+          assign_serialized_attributes(name, associated)
+        end
+
+        # Getter, e.g. object.creator
+        #
+        define_method("#{name}") do
+          instance_variable_get("@#{name}") or
+          instance_variable_set("@#{name}", deserialize(name)) or
+          instance_variable_set("@#{name}", load_associated(name))
+        end
+
+
+        # Force a re/load of the association from the database,
+        # overwriting any memoized version.
+        #
+        define_method("full_#{name}") do
+          instance_variable_set("@#{name}", load_associated(name))
+        end
+      end
+    end
+
+
+    # Load the association from the database.
+    #
+    def load_associated(name)
+      return nil unless association_id = send("#{name}_id")
+
+      klass = default_class_for(name)
+      primary_key = default_primary_key_for(name)
+
+      klass.where(primary_key => association_id).first
+    end
+
+    private
+
+    # Assign the association id based on default primary key
+    #
+    def assign_id_for(name, associated)
+      primary_key = default_primary_key_for(name)
+      id = associated ? associated[primary_key] : nil
+
+      send("#{name}_id=", id)
+    end
+
+
+    # Serialize and assign the association
+    #
+    def assign_serialized_attributes(name, associated)
+      serialized = associated ? serialize(name, associated) : {}
+
+      send("stored_#{name}=", serialized)
+    end
+
+
+    # Get the class name from the config options, e.g.
+    # Redcrumbs.creator_class_sym
+    #
+    def default_class_for(name)
+      Redcrumbs.send("#{name}_class_sym").to_s.classify.constantize
+    end
+
+
+    # Get the expected primary key for the association from
+    # the config options.
+    #
+    def default_primary_key_for(name)
+      Redcrumbs.send("#{name}_primary_key")
+    end
+
+
+    # Serializes a given object using the configuration options
+    # for the association.
+    #
+    def serialize(name, associated)
+      keys = Redcrumbs.send("store_#{name}_attributes").dup
+
+      associated.attributes.select {|k,v| keys.include?(k.to_sym)}
+    end
+
+
+    # Returns a new instance of the associated object based on the
+    # serialized attributes only.
+    #
+    def deserialize(name)
+      properties = send("stored_#{name}")
+
+      return nil unless properties.present?
+
+      default_class_for(name).new(properties, :without_protection => true)
+    end
+  end
+end
