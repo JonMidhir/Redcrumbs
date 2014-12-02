aaf9ac3af3c0b11d46dee82a8a9341cb611f8659
John Hope <info@midhirrecords.com>
2014-12-02 12:33:13 +0000

    Removes deprecated primary_key settings and warns

--- lib/redcrumbs/config.rb
+++ lib/redcrumbs/config.rb
@@ -1,8 +1,6 @@
 module Redcrumbs
   mattr_accessor :creator_class_sym
-  mattr_accessor :creator_primary_key
   mattr_accessor :target_class_sym
-  mattr_accessor :target_primary_key
 
   mattr_accessor :store_creator_attributes
   mattr_accessor :store_target_attributes
@@ -17,16 +15,27 @@ module Redcrumbs
   # of the gem, in future require an explicit creator/target method to set.
   #
   @@creator_class_sym ||= :user
-  # @@creator_primary_key ||= 'id'
   @@target_class_sym ||= :user 
-  # @@target_primary_key ||= 'id'
 
   @@store_creator_attributes ||= []
   @@store_target_attributes ||= []
 
+  def self.creator_primary_key
+  end
+
+  def self.target_primary_key
+  end
+
+  def self.creator_primary_key=
+    warn 'Setting creator primary_key is no longer possible. Model primary_key will be used.'
+  end
+
+  def self.target_primary_key=
+    warn 'Setting target primary_key is no longer possible. Model primary_key will be used.'
+  end
 
   def self.primary_key_for(association_name)
-    raise ArgumentError unless [:creator, :target].include?(association_name)
+    return nil unless [:creator, :target].include?(association_name)
 
     send("#{association_name}_primary_key")
   end
